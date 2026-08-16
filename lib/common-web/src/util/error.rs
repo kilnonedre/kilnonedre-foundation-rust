79e64e8286b0ef51940f5e86581df9ea63a09eeb
kilnonedre <kilnonedre@MacBook-Pro.local>
2026-08-16 08:57:56 +0800

    fix(web): 报错位置

--- lib/common-web/src/util/error.rs
+++ lib/common-web/src/util/error.rs
@@ -1,5 +1,6 @@
 use crate::ApiError;
 
+#[track_caller]
 pub fn svc_err_internal<E>(e: E, msg: &str) -> ApiError
 where
     E: std::error::Error,
@@ -8,11 +9,13 @@ where
     ApiError::Internal
 }
 
+#[track_caller]
 pub fn svc_err_internal_msg(msg: &str) -> ApiError {
     log::error!("❌ {}", msg);
     ApiError::Internal
 }
 
+#[track_caller]
 pub fn svc_err_bad_request<E>(e: E, svc: u16, err_no: u16, msg: &str) -> ApiError
 where
     E: std::error::Error,
@@ -25,6 +28,7 @@ where
     }
 }
 
+#[track_caller]
 pub fn svc_err_bad_request_msg(svc: u16, err_no: u16, msg: &str) -> ApiError {
     log::warn!("⚠️ {}", msg);
     ApiError::BadRequest {
