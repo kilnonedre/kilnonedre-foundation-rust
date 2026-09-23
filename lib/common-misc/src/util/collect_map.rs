1817b1fe5a4900bcc41f50de8e200e34f891746a
kilnonedre <kilnonedre@MacBook-Pro.local>
2026-09-23 09:56:26 +0800

    feat(misc): collect_map add opt

--- lib/common-misc/src/util/collect_map.rs
+++ lib/common-misc/src/util/collect_map.rs
@@ -13,3 +13,27 @@ where
 {
     values.iter().cloned().map(mapper).collect()
 }
+
+pub fn collect_map_opt<T, R, F>(values: &Option<Vec<T>>, mapper: F) -> Option<Vec<R>>
+where
+    T: Clone,
+    F: Fn(T) -> R,
+{
+    values
+        .as_ref()
+        .map(|values| values.iter().cloned().map(mapper).collect())
+}
+
+pub fn try_collect_map_opt<T, R, E, F>(
+    values: &Option<Vec<T>>,
+    mapper: F,
+) -> Result<Option<Vec<R>>, E>
+where
+    T: Clone,
+    F: Fn(T) -> Result<R, E>,
+{
+    values
+        .as_ref()
+        .map(|values| values.iter().cloned().map(mapper).collect())
+        .transpose()
+}
